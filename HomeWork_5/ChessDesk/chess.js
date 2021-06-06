49158a1511a7e7ff5459ae38271a6e11d4e57e4f
Anton Sobolev <antonsobolev1987@gmail.com>
2021-06-06 22:33:17 +0900

    сделал 1 задание

--- HomeWork_5/ChessDesk/chess.js
+++ HomeWork_5/ChessDesk/chess.js
@@ -1,30 +1,77 @@
-function generateChess() {
-  let mainContainer = document.querySelector(".mainContainer"); //Переменная для вывода блока, содержащего элементы
-  let container; //переменная для хранения созданных элементов
-  let label = true; //переменная-метка для определения цвета
+//шахматное поле генерируется размером 10х10 - 8 строк игровое поле и по одной строке сверху и снизу для вывода цифр. Тот же принцип со столбцами.
 
-  for (let x = 9; x >= 9; x--) {
-    //цикл для строк (8 игровых, 2 для меток
-    for (let y = 0; y < 10; y++) {
-      if (y == 0) label = !label; // условие если началась новая строка, отрицай значение метки, чтобы счередовать порядок цвета
-      container = document.createElement("div"); //присвоил переменной элемента созданный элемент div
-      if (label) container.className = "black container";
-      //если переменная-метка true, присвой имя класса контейнеру "Черный"
-      else container.className = "white container"; //иначе, имя класса "Белый"
-      mainContainer.appendChild(container); //добавил созданный элемент div в главный контейнер
+var container = document.createElement("div"); // создаю контейнер
+container.className = "container"; //присваиваю ему класс container
+container.id = "container"; //присваиваю ему идентификатор container
+document.body.appendChild(container); // встраиваю его в DOM, в тег body
 
-      label = !label; // присвоил переменной-метке, отрицание самой себя, для смены цвета
+var chessDesk = document.createElement("div"); //создаю div для шахматной доски
+chessDesk.className = "chessDesk"; //присваиваю ему класс
+chessDesk.id = "chessDesk"; //присваиваю ему идентификатор
+chessDesk.innerHTML = ""; //Добавляю в него пустой контент
+container.appendChild(chessDesk); // встраиваю его в DOM, в тег container
+
+var x, y, chessDeskCell, letter; //объявляю переменную x для строк, y для ячеек, chessDeskCell для создания div'а каждой ячейке, letter для хранения букв столбцов
+for (x = 9; x >= 0; x--) {
+  // запускаю цикл для строк
+
+  for (y = 0; y < 10; y++) {
+    // запускаю цикл для ячеек
+
+    chessDeskCell = document.createElement("div"); //создаю в переменной новыю ячейку
+    chessDeskCell.className = "chessDeskCell"; //присваиваю ей класс
+    chessDeskCell.id = "" + x + y; // присваиваю ей индекс
+
+    //определяю размеры игрового поля
+    if (y === 0 || y === 9 || x === 9 || x === 0) {
+      // если ячейка находится на 0 или 9 месте, то она находится за пределами игрового поля, а значит нужно проставить названия строк и столбцов
+
+      if ((y === 0 || y === 9) && x !== 0 && x !== 9) {
+        //если ячейка по вертикали находится между 0 и 9 значением строк, то
+        chessDeskCell.className += " chessDeskCell_index"; //добавляю ей класс индекса
+        chessDeskCell.innerHTML = " " + x; //и добавляю к пустому контенту порядковый номер строки, на которой она находится
+      } else if ((x === 0 || x === 9) && y !== 0 && y !== 9) {
+        //иначе эта ячейка находится не по вертикали а горизонтали, а значит выводим буквы
+        switch (y) {
+          case 1:
+            letterName = "A";
+            break;
+          case 2:
+            letterName = "B";
+            break;
+          case 3:
+            letterName = "C";
+            break;
+          case 4:
+            letterName = "D";
+            break;
+          case 5:
+            letterName = "E";
+            break;
+          case 6:
+            letterName = "F";
+            break;
+          case 7:
+            letterName = "G";
+            break;
+          case 8:
+            letterName = "H";
+            break;
+        }
+
+        chessDeskCell.className += " chessDeskCell_index"; //присваиваю индекс ячейке в столбце
+        chessDeskCell.innerHTML = " " + letterName; //добавляю контент с названием столбцов
+      }
+    } else {
+      // определяю черные и белые ячейки
+
+      if ((x + y) % 2 === 0) {
+        chessDeskCell.className += " chessDeskCell_black";
+      } else {
+        chessDeskCell.className += " chessDeskCell_white";
+      }
     }
+
+    chessDesk.appendChild(chessDeskCell); //добавляю ячейку в DOM к элементу доски
   }
-
-  var row = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  var col = ["1", "2", "3", "4", "5", "6", "7", "8"];
-
-  const chessDesk = document.getElementsByClassName("container");
-  const arrayDiv = Array.from(chessDesk);
-  arrayDiv[0][0] = "1";
-  console.log(arrayDiv);
-  document.write(arrayDiv);
 }
-
-generateChess();
